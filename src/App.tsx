6df6476fb6d7be584f3832c252ff35d805ddf519
Kunal Kishore <kunalkishore1807@gmail.com>
2023-06-06 12:36:25 +0530

    feat(Chat section added in the app.tsx and some styling is done to make iot look good):

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import ChatHeader from './components/ChatHeader';
-import ChatDescription from './components/ChatDescription';
+import React from "react";
+import ChatHeader from "./components/ChatHeader";
+import ChatDescription from "./components/ChatDescription";
+import ChatSection from "./components/ChatSection";
 
 function App() {
   return (
     <div>
-      <ChatHeader/>
-      <ChatDescription/>
+      <div
+        style={{ position: "fixed", top: "0px", left: "0px", zIndex: "999", width:'100%', background: '#FAF9F4', height:'20%' }}
+      >
+        <ChatHeader />
+        <ChatDescription />
+      </div>
+      <div style={{marginTop: '12em'}}>
+        <ChatSection />
+      </div>
     </div>
   );
 }
